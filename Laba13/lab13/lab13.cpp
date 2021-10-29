c0432a737f03e6f84ad7525761d0dc00d8daec77
Dev1lShark <91787831+Dev1lShark@users.noreply.github.com>
2021-10-29 09:09:14 +0300

    Add files via upload

---
+++ Laba13/lab13/lab13.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include "Car.h"
+#include "Lorry.h"
+using namespace std;
+void f1(Car& c)
+{
+	c.Setmark("skoda"); cout << c;
+}
+
+Car f2()
+{
+	Lorry l("Mercedes", 1, 2, 3);
+	return l;
+}
+
+int main()
+{
+	Car a;
+	cin >> a;
+	cout << a;
+	Car b("BMW", 4, 115);
+	cout << b;
+	a = b;
+	cout << a;
+	a = f2();
+	cout << a;
+	Lorry c; //создать объект
+	cin >> c;//ввести значения атрибутов
+	cout << c;//вывести значенияатрибутов
+	system("pause");
+}

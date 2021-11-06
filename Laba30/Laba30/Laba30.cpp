72d1ae2af50a0988d2304b1732bc7ab7b90ec2c3
Dev1lShark <91787831+Dev1lShark@users.noreply.github.com>
2021-11-06 22:43:30 +0300

    Add files via upload

---
+++ Laba30/Laba30/Laba30.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	float a, b, c;
+		cout << "\na = "; cin >> a;
+		cout << "\nb = "; cin >> b;
+		cout << "\nc = "; cin >> c;
+		if ((a <= 0) || (b <= 0) || (c <= 0) || (a + b < c) || (a + c < b) || (b + c < a))
+			cout << "\n Некорректно введённые данные";
+		else if (a == b || a == c || b == c)
+			cout << "\n Треугольник равнобедренный";
+		else 
+			cout << "\n Треугольник не равнобедренный.";
+}

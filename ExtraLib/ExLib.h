dc6e8d7bdb3c067977a86e95f6c23cf5c864c1e1
Dev1lShark <91787831+Dev1lShark@users.noreply.github.com>
2021-12-22 20:35:11 +0300

    Add files via upload

---
+++ ExtraLib/ExLib.h
@@ -0,0 +1,13 @@
+#pragma once
+namespace ExLib
+{
+    class Header
+    {
+    public:
+        static void Name();
+        static void Group();
+        static void Day();
+        static void Time();
+        static void file();
+    };
+}
